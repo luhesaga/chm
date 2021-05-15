ea97642f06e2a7bf454e898c5a34233d07e14f92
Luis Sarmiento <sarmientoluish@americana.edu.co>
2021-05-14 20:44:06 -0500

    avance 2 examenes

--- src/app/dashboard/components/exercises/exercises-create/exercises-create.component.ts
+++ src/app/dashboard/components/exercises/exercises-create/exercises-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'app-exercises-create',
@@ -7,9 +8,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExercisesCreateComponent implements OnInit {
 
+  selected: number;
+  pivSelected: number;
+
+  options = [
+    {
+      name: 'Selección única',
+      img: '/assets/icons/respuesta_unica.svg',
+      value: 1,
+      state: false
+    },
+    {
+      name: 'Selección multiple',
+      img: '/assets/icons/respuesta_multiple.svg',
+      value: 2,
+      state: false
+    },
+    {
+      name: 'Rellenar blancos',
+      img: '/assets/icons/rellenar_blancos.svg',
+      value: 3,
+      state: false
+    },
+    {
+      name: 'Relacionar',
+      img: '/assets/icons/relacionar.svg',
+      value: 4,
+      state: false
+    },
+    {
+      name: 'Respuesta libre',
+      img: '/assets/icons/respuesta_libre.svg',
+      value: 5,
+      state: false
+    }
+  ]
+
+  answers = [
+    {
+      value: 1,
+      true: false,
+      punctuation: 0,
+      answer: '',
+      comment: '',
+    },
+    {
+      value: 2,
+      true: false,
+      punctuation: 0,
+      answer: '',
+      comment: '',
+    },
+    {
+      value: 3,
+      true: false,
+      punctuation: 0,
+      answer: '',
+      comment: '',
+    },
+    {
+      value: 4,
+      true: false,
+      punctuation: 0,
+      answer: '',
+      comment: '',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectionChange(event: MatRadioChange) {
+
+    let pos;
+
+    if (this.pivSelected) {
+      pos = this.pivSelected - 1;
+      this.options[pos].state = false;
+    }
+    this.pivSelected = this.selected;
+
+    pos = event.value - 1;
+    console.log(this.options[pos].name);
+    this.options[pos].state = true;
+
   }
 
 }
