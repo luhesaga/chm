1dc71645673b2706f3e760b99c38c123a2c4249a
Luis Sarmiento <sarmientoluish@americana.edu.co>
2022-12-28 21:18:33 -0500

    Cambio de año en el footer

--- src/app/home/components/footer/footer.component.ts
+++ src/app/home/components/footer/footer.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  year = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.year = new Date().getFullYear();
   }
 
 }
